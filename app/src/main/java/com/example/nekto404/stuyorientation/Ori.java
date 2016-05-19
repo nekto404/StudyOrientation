6aafc35141ff26855407e9994dfb0cf7e7334509
Nekto404 <gsmartrsh@gmail.com>
2016-05-19 20:15:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nekto404/stuyorientation/Ori.java
@@ -0,0 +1,13 @@
+package com.example.nekto404.stuyorientation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ori extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ori);
+    }
+}
